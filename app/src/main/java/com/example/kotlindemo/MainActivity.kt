8cac2282e0c49b4b506567ac97a0a6e541445f30
Apurva Patel <apurvapatel@Apurvas-Mac-mini.local>
2022-11-04 00:13:44 +0530

    First Hello World Change

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.kotlindemo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button2 :Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button2 = findViewById(R.id.button2)
+        button2.text="Hello World"
     }
 }
