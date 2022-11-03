08842d45e88422afb8682625613232d27077a89a
Apurva Patel <apurvapatel@Apurvas-Mac-mini.local>
2022-11-04 00:18:46 +0530

    aputva-one first commit

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -14,6 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button2 = findViewById(R.id.button2)
-        button2.text="Hello World"
+        button2.text="Hello World!!!!!"
     }
 }
